dca069e35cce12808c44f6713fba5019916d8839
spotykatch <andrey.dashevskiy@yandex.ru>
2023-05-03 02:00:15 +0300

    that does not work

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  webpack: (config, { webpack }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: false,
+      path: false,
+      "react-native-sqlite-storage": false,
+      "aws-sdk": false
+    };
+    config.plugins = [
+      ...config.plugins,
+      new webpack.NormalModuleReplacementPlugin(/typeorm$/, function (result) {
+        result.request = result.request.replace(/typeorm/, "typeorm/browser");
+      }),
+      new webpack.ProvidePlugin({
+        "window.SQL": "sql.js/dist/sql-wasm.js",
+      }),
+    ];
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
